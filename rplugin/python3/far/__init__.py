bdea0fa77b3c0a67f50e21b244b9ff1b119e0028
brooth <brooth@gmail.com>
2017-07-04 08:32:07 +0600

    fix: ImportError: No module named 'neovim' when I use ag as far's source #28

--- rplugin/python3/far/__init__.py
+++ rplugin/python3/far/__init__.py
@@ -5,36 +5,41 @@ Author: Oleg Khalidov <brooth@gmail.com>
 License: MIT
 """
 
-import neovim
-import logging
-import importlib
+try:
+    import neovim
+    import logging
+    import importlib
 
-logger = logging.getLogger('far')
-
-
-@neovim.plugin
-class FarPlugin(object):
-
-    def __init__(self, nvim):
-        self.nvim = nvim
-
-        if nvim.eval('far#tools#isdebug()'):
-            logger.addHandler(NeoVimLoggerHandler(nvim))
-            logger.setLevel(logging.DEBUG)
-
-    @neovim.function('_far_nvim_rpc_async_invoke', sync=False)
-    def _far_nvim_rpc_invoke(self, args):
-
-        logger.debug('_far_nvim_rpc_invoke(%s)', str(args))
-        for execline in args[0]:
-            exec(execline)
+    logger = logging.getLogger('far')
 
 
-class NeoVimLoggerHandler(logging.Handler):
-    def __init__(self, nvim):
-        super().__init__()
-        self.nvim = nvim
+    @neovim.plugin
+    class FarPlugin(object):
 
-    def emit(self, record):
-        msg = self.format(record).replace('"', '`')
-        self.nvim.command('call far#tools#log("farnvim:' + msg + '")')
+        def __init__(self, nvim):
+            self.nvim = nvim
+
+            if nvim.eval('far#tools#isdebug()'):
+                logger.addHandler(NeoVimLoggerHandler(nvim))
+                logger.setLevel(logging.DEBUG)
+
+        @neovim.function('_far_nvim_rpc_async_invoke', sync=False)
+        def _far_nvim_rpc_invoke(self, args):
+
+            logger.debug('_far_nvim_rpc_invoke(%s)', str(args))
+            for execline in args[0]:
+                exec(execline)
+
+
+    class NeoVimLoggerHandler(logging.Handler):
+        def __init__(self, nvim):
+            super().__init__()
+            self.nvim = nvim
+
+        def emit(self, record):
+            msg = self.format(record).replace('"', '`')
+            self.nvim.command('call far#tools#log("farnvim:' + msg + '")')
+
+except ImportError:
+    pass
+
